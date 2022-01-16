ff5b85e85eda4d4f483c4b5293030d0d9041bac7
Yimura <andreas.maerten@scarlet.be>
2022-01-16 13:56:25 +0100

    fix(Module): Fixed exports

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ import ModuleBuilder from './src/ModuleBuilder.js';
 import ModuleManager from './src/ModuleManager.js';
 
 export default ModuleManager;
-
-export const ModuleBuilder;
-export const ModuleManager;
+export { ModuleBuilder, ModuleManager };
